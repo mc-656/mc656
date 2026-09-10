2ab657492833a1068a1d0a4cbe143b41bce8cac8
Miguel <miguelpereiraramos1108@gmail.com>
2026-09-10 09:34:51 -0300

    config: inicialização do Spring

---
+++ src/test/java/com/unicamp/engsoft/eleicao/EleicaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unicamp.engsoft.eleicao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EleicaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
