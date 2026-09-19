6a7f1020d484d7845eeae9d4ddef799626aeaf17
Miguel <miguelpereiraramos1108@gmail.com>
2026-09-19 17:15:13 -0300

    feat: adição de exceptions básicas para popular a pasta (conforme detalhado na issue)

---
+++ src/main/java/com/unicamp/engsoft/eleicao/shared/exception/RecursoNaoEncontradoException.java
@@ -0,0 +1,15 @@
+package com.unicamp.engsoft.eleicao.shared.exception;
+
+import org.springframework.http.HttpStatus;
+
+/** Recurso solicitado não existe. Mapeada para 404. */
+public class RecursoNaoEncontradoException extends DomainException {
+
+	public RecursoNaoEncontradoException(String recurso, Object identificador) {
+		super("%s não encontrado(a): %s".formatted(recurso, identificador), HttpStatus.NOT_FOUND);
+	}
+
+	public RecursoNaoEncontradoException(String mensagem) {
+		super(mensagem, HttpStatus.NOT_FOUND);
+	}
+}
