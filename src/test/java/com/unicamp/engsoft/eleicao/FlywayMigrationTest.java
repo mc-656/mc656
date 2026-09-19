264429d2413e05084037705d336ffb8dc6627453
Miguel <miguelpereiraramos1108@gmail.com>
2026-09-19 18:18:22 -0300

    fix: conserta nome da classe

--- src/test/java/com/unicamp/engsoft/eleicao/FlywayMigrationTest.java
+++ src/test/java/com/unicamp/engsoft/eleicao/FlywayMigrationTest.java
@@ -7,7 +7,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-class FlywayMigrationIT extends AbstractIntegrationTest {
+class FlywayMigrationTest extends AbstractIntegrationTest {
 
     @Autowired
     Flyway flyway;
