3188e433ea9b8bb6e08f84a786e5100803624aec
Caio <caiomaia2005.ms@gmail.com>
2026-09-20 19:03:48 -0300

    fix-remove @Testcontainers e @Container para lidar com erro de Connection Refused

--- src/test/java/com/unicamp/engsoft/eleicao/AbstractIntegrationTest.java
+++ src/test/java/com/unicamp/engsoft/eleicao/AbstractIntegrationTest.java
@@ -3,13 +3,14 @@ package com.unicamp.engsoft.eleicao;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
-@Testcontainers
 public abstract class AbstractIntegrationTest {
 
-    @Container @ServiceConnection
+    @ServiceConnection
     static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:17-alpine");
+
+    static {
+        postgres.start();
+    }
 }
