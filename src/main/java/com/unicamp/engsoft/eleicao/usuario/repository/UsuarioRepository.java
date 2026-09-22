2047668aeb3c27514098f97f9f6506f320d677fd
Miguel <miguelpereiraramos1108@gmail.com>
2026-09-22 08:47:59 -0300

    feat: atualiza repository

--- src/main/java/com/unicamp/engsoft/eleicao/usuario/repository/UsuarioRepository.java
+++ src/main/java/com/unicamp/engsoft/eleicao/usuario/repository/UsuarioRepository.java
@@ -4,13 +4,13 @@ import com.unicamp.engsoft.eleicao.usuario.domain.Usuario;
 import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, UUID> {
     boolean existsByCpf(String cpf);
 
     boolean existsByEmail(String email);
+
+    Optional<Usuario> findByEmail(String email);
 
     Optional<Usuario> findByCpf(String cpf);
 }
